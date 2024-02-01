ee2d00e6d4ff94f2fd0502670f9a17a83833c35f
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-01 19:25:33 +0100

    owner change

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#  script that sets up your web servers for the deployment of web_static.
+# script that sets up your web servers for the deployment of web_static.
 
 sudo apt-get update
 sudo apt-get -y install nginx
@@ -7,12 +7,13 @@ sudo service nginx start
 
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
-echo "Hello Worl!" | sudo tee /data/web_static/releases/test/index.html > dev/null
+echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html > dev/null
 
 # create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chowm -R Ubuntu:ubuntu /data/
+# change ownership for the folder to the server user
+sudo chowm -R ubuntu:ubuntu /data/
 
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
