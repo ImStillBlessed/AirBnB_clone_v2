ce961ec8ff1ea9dc9a510b30a03547786859ff90
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-02 20:22:52 +0100

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This is  a Fabric script that generates a .tgz archive
+from the contents of the web_static folder of your AirBnB Clone repo,
+using the function do_pack.
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    stores all the files in web_static folder in tbz archive
+    """
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'version/web_static/' + now + '.tgz'
+
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    if result.return_code == 0:
+        return archive_path
+    return None
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to your web servers.
+    """
+    if not archive_path:
+        return False
