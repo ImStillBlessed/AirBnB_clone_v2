f01792e820710e18f3c6ad20b637fb1782c9b61c
Blessed Oigbochie <eprince1800@gmail.com>
2024-02-04 01:20:45 -0800

    usin sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,13 +23,13 @@ def do_deploy(archive_path):
         file_name = archive_path.split("/")[-1]
         file_no_ext = file_name.split(".")[0]
         remote_path = "/data/web_static/releases/{}/".format(file_no_ext)
-        run("mkdir -p {}".format(remote_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(remote_path, remote_path))
-        run("rm -rf {}web_static".format(remote_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(remote_path))
+        run("sudo mkdir -p {}".format(remote_path))
+        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
+        run("sudo rm /tmp/{}".format(file_name))
+        run("sudo mv {}web_static/* {}".format(remote_path, remote_path))
+        run("sudo rm -rf {}web_static".format(remote_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(remote_path))
         print("New version deployed!")
         return True
     except Exception as e:
