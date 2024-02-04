82fa01ef3d1b63505b863f3b88cbda6794c966aa
Blessed Oigbochie <106008185+ImStillBlessed@users.noreply.github.com>
2024-02-04 10:52:42 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-Compress web static package
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy.
 """
+
 from fabric.api import *
 from os import path
 
@@ -14,36 +16,22 @@ def do_deploy(archive_path):
     Distributes an archive to your web servers
     """
     if not path.exists(archive_path):
-        print(f"Error: Archive '{archive_path}' not found.")
         return False
 
     try:
-        # Extract file name and file name without extension
-        file_name = path.basename(archive_path)
-        file_no_ext = path.splitext(file_name)[0]
-
-        # Remote path for deployment
-        remote_path = "/data/web_static/releases/{}/".format(file_no_ext)
-
-        # Upload archive
         put(archive_path, "/tmp/")
-
-        # Create directory and extract archive
+        file_name = archive_path.split("/")[-1]
+        file_no_ext = file_name.split(".")[0]
+        remote_path = "/data/web_static/releases/{}/".format(file_no_ext)
         run("sudo mkdir -p {}".format(remote_path))
         run("sudo tar -xzf /tmp/{} -C {}".format(file_name, remote_path))
-
-        # Remove archive from /tmp
         run("sudo rm /tmp/{}".format(file_name))
-
-        # Use rsync to move contents, remove unnecessary directory, and create symlink
-        run("sudo rsync -av --ignore-existing {}/web_static/ {}/".format(remote_path, remote_path))
+        run("sudo mv -f {}web_static/* {}".format(remote_path, remote_path))
         run("sudo rm -rf {}web_static".format(remote_path))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(remote_path))
-
         print("New version deployed!")
         return True
     except Exception as e:
-        print(f"Error: {e}")
+        print(e)
         return False
-        
