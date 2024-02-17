3573323487cfd098fe9ac0cc6a5aadb2f289560e
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-18 00:51:05 +0100

    task 9 doen

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script defines routes for a Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+# storage.all()
+
+
+@app.teardown_appcontext
+def closing(arg=None):
+    """
+    Method For handling closing of each session
+    """
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("states/<id>", strict_slashes=False)
+def state_id_filter(id=None):
+    """
+    Displays state in html if the object is found with this id
+    """
+    path = '9-states.html'
+    states = storage.all(State)
+    return render_template(path, states=states, id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
